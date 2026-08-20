e0d9cb5255af5069116a328f5255e2bcbbe3d17c
kimzuni <me@kimzuni.com>
2026-08-20 22:08:10 +0900

    refactor(cli): use targeted `.mockRestore()` instead of global `mock.restore()`

--- packages/@kimzuni/templify-cli/tests/preload.ts
+++ packages/@kimzuni/templify-cli/tests/preload.ts
@@ -1,28 +1,31 @@
 import fs from "node:fs/promises";
-import { mock, beforeAll, afterAll, spyOn } from "bun:test";
+import { type Mock, beforeAll, afterAll, spyOn } from "bun:test";
 
 import { tempdir, captureStack } from "./common";
 
 
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mocks: Array<Mock<(...args: any[]) => any>> = [];
+
 function spy() {
-	spyOn(console, "log").mockImplementation((...args) => captureStack.at(-1)?.log.push(args));
-	spyOn(console, "error").mockImplementation((...args) => captureStack.at(-1)?.error.push(args));
+	mocks.push(spyOn(console, "log").mockImplementation((...args) => captureStack.at(-1)?.log.push(args)));
+	mocks.push(spyOn(console, "error").mockImplementation((...args) => captureStack.at(-1)?.error.push(args)));
 
-	spyOn(process.stdout, "write").mockImplementation((output) => {
+	mocks.push(spyOn(process.stdout, "write").mockImplementation((output) => {
 		captureStack.at(-1)?.log.push([output.toString()]);
 		return true;
-	});
-	spyOn(process.stderr, "write").mockImplementation((output) => {
+	}));
+	mocks.push(spyOn(process.stderr, "write").mockImplementation((output) => {
 		captureStack.at(-1)?.log.push([output.toString()]);
 		return true;
-	});
+	}));
 
 	// @ts-expect-error: ts(2345)
-	spyOn(process.stdin, "isTTY").mockReturnValue(true);
+	mocks.push(spyOn(process.stdin, "isTTY").mockReturnValue(true));
 
 	// @ts-expect-error: ts(2345)
-	spyOn(process.stdout, "isTTY").mockReturnValue(true);
+	mocks.push(spyOn(process.stdout, "isTTY").mockReturnValue(true));
 }
 
 async function before() {
@@ -31,7 +34,7 @@ async function before() {
 }
 async function after() {
 	await fs.rm(tempdir, { recursive: true });
-	mock.restore();
+	for (const x of mocks) x.mockRestore();
 }
 
 
