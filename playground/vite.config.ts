798fb1de41e09850321f620c2926328a7f64b29f
kimzuni <me@kimzuni.com>
2026-08-22 23:18:22 +0900

    ci(playground): setup github pages deployment workflow and fix build script
    - add npm-run-all and apply with lint script

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base   : "/templify/",
 	plugins: [
 		react(),
 		babel({ presets: [reactCompilerPreset()] }),
